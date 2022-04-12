c4a3ed254770c2afdb60ef47c6ff0fd7438d54b2
Owen Waring <bizartist@gmail.com>
2022-04-12 15:09:25 -0700

    isPalindrome, findIndex, and revString working

--- recursion.js
+++ recursion.js
@@ -21,25 +21,38 @@ function longest(words) {
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str) {
-  if (str.length === 0) return "";
+	if (str.length === 0) return '';
 
-  return str[0]+everyOther(str.slice(2));
+	return str[0] + everyOther(str.slice(2));
 }
-
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
+	if (str.length < 2) return true;
+	if (str[0] !== str[str.length - 1]) return false;
 
+	return isPalindrome(str.slice(1, str.length - 1));
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {}
+function findIndex(arr, val) {
+  if (arr.length === 0) return -1;
+  if (arr[0] === val) return 0;
+
+  let found = findIndex(arr.slice(1), val);
+
+  return found !== -1 ?  1 + found : -1;
+}
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str) {
+  if (str.length === 0) return "";
+
+  return revString(str.slice(1)) + str[0];
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
